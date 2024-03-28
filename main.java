110ae59f8b8ea17096bf7d3d51d519acf18f211c
RAHAT <105932806+rahat2924@users.noreply.github.com>
2024-03-28 23:02:17 +0600

    Create main.java

---
+++ main.java
@@ -0,0 +1,21 @@
+abstract class Shape3D {
+ public abstract double
+ calculateVolume();
+ public abstract double
+ calculateSurfaceArea();
+ }
+ class Sphere extends Shape3D {
+ // Implement calculateVolume() and
+ calculateSurfaceArea() for sphere
+ }
+ class Cube extends Shape3D {
+ // Implement calculateVolume() and
+ calculateSurfaceArea() for cube
+ }
+ public class Main {
+ public static void main(String[]
+ args) {
+ // Code to test Sphere and Cube
+classes
+ }
+ }
